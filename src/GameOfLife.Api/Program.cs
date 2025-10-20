b9a13184355a7eac7395c14ac09fbe624a8fb255
Jean Lima <jean.lima@lynnco.com>
2025-10-19 21:51:57 -0300

    tests: Update integrated tests

--- src/GameOfLife.Api/Program.cs
+++ src/GameOfLife.Api/Program.cs
@@ -4,47 +4,61 @@ using GameOfLife.Core.Services;
 using GameOfLife.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<GameOfLifeSettings>(
-    builder.Configuration.GetSection("GameOfLifeSettings"));
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<GameOfLifeContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddScoped<IGameOfLifeService, GameOfLifeService>();
-builder.Services.AddScoped<IBoardRepository, BoardRepository>();
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.SwaggerDoc("v1", new() { Title = "Game of Life API", Version = "v1" });
-});
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+        builder.Services.Configure<GameOfLifeSettings>(
+            builder.Configuration.GetSection("GameOfLifeSettings"));
 
-var app = builder.Build();
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<GameOfLifeContext>(options =>
+            options.UseSqlServer(connectionString));
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddScoped<IGameOfLifeService, GameOfLifeService>();
+        builder.Services.AddScoped<IBoardRepository, BoardRepository>();
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new() { Title = "Game of Life API", Version = "v1" });
+        });
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<GameOfLifeContext>();
+            var databaseProvider = dbContext.Database.ProviderName;
+            if (databaseProvider != "Microsoft.EntityFrameworkCore.InMemory")
+            {
+                dbContext.Database.Migrate();
+            }
+            else
+            {
+                dbContext.Database.EnsureCreated();
+            }
+        }
+
+        app.Run();
+    }
 }
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<GameOfLifeContext>();
-    dbContext.Database.Migrate();
-}
-
-app.Run();
--- tests/GameOfLife.Api.IntegrationTests/GameOfLife.Api.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/GameOfLife.Api.IntegrationTests/GameOfLife.Api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,7 +1,8 @@
+using GameOfLife.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using GameOfLife.Infrastructure.Data;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
@@ -9,19 +10,16 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
         builder.ConfigureServices(services =>
         {
-            // Remove the existing DbContext registration
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<GameOfLifeContext>));
             if (descriptor != null)
                 services.Remove(descriptor);
 
-            // Add DbContext with InMemory provider for tests
             services.AddDbContext<GameOfLifeContext>(options =>
             {
                 options.UseInMemoryDatabase("TestDb");
             });
 
-            // Ensure database is created
             var sp = services.BuildServiceProvider();
             using var scope = sp.CreateScope();
             var db = scope.ServiceProvider.GetRequiredService<GameOfLifeContext>();
